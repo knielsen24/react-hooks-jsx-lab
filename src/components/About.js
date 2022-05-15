211dccf43b392ba83576e68a27a3f25be17bd246
Kevin Nielsen <knielsen24@gmail.com>
2022-05-15 13:11:09 -0700

    commit tree 00f5a15afe965fa3619990cac910b58f0b24304d

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">About
+      <h2>About Me</h2>
+      <p>Born and raised in Ventura County, Kevin's passion for technology started at an early age.
+        That passion was sparked with playing sonic with his cousins</p>
+      <img src={ image } alt="I made this" />
+    </div>)
+  ;
 }
 
 export default About;
